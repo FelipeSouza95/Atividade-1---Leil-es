e6ea6a31192abea7793c5ca9ff674094e059a2fb
FelipeSouza95 <felipemoraes.recreio@gmail.com>
2024-08-19 11:26:59 -0300

    Configuração botão cadastro

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -14,7 +14,21 @@ public class ProdutosDAO {
 
     public void cadastrarProduto(ProdutosDTO produto) {
 
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
+        
+        String sql = "INSERT INTO PRODUTOS (nome,valor,status) VALUES (?,?,?)";
+        
+        try {
+            PreparedStatement p = conn.prepareStatement(sql);
+            p.setString(1, produto.getNome());
+            p.setInt(2, produto.getValor());
+            p.setString(3, produto.getStatus());
+            p.executeUpdate();
+            
+            JOptionPane.showMessageDialog(null, "Produto Cadastrado com Sucesso");
+        } catch (Exception e) {
+            System.out.println("Erro ao cadastrar." + e.getMessage());
+        }
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
