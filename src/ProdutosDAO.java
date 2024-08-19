ac932c2861dc75428430b2c2b4197c9c354cb730
FelipeSouza95 <felipemoraes.recreio@gmail.com>
2024-08-19 14:27:40 -0300

    Configuração de listagem de produtos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 public class ProdutosDAO {
 
@@ -15,16 +16,16 @@ public class ProdutosDAO {
     public void cadastrarProduto(ProdutosDTO produto) {
 
         conn = new conectaDAO().connectDB();
-        
+
         String sql = "INSERT INTO PRODUTOS (nome,valor,status) VALUES (?,?,?)";
-        
+
         try {
             PreparedStatement p = conn.prepareStatement(sql);
             p.setString(1, produto.getNome());
             p.setInt(2, produto.getValor());
             p.setString(3, produto.getStatus());
             p.executeUpdate();
-            
+
             JOptionPane.showMessageDialog(null, "Produto Cadastrado com Sucesso");
         } catch (Exception e) {
             System.out.println("Erro ao cadastrar." + e.getMessage());
@@ -33,7 +34,39 @@ public class ProdutosDAO {
 
     public ArrayList<ProdutosDTO> listarProdutos() {
 
-        return listagem;
+        ArrayList<ProdutosDTO> lista = new ArrayList<>();
+        conn = new conectaDAO().connectDB(); // Utiliza a classe conectaDAO para obter a conexão
+
+        String sql = "SELECT * FROM PRODUTOS";
+
+        try {
+            PreparedStatement p = conn.prepareStatement(sql);
+            resultset = p.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                lista.add(produto);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + e.getMessage());
+        } finally {
+            try {
+                if (resultset != null) {
+                    resultset.close(); // Fecha o ResultSet no bloco finally
+                }
+                if (conn != null) {
+                    conn.close(); // Fecha a conexão no bloco finally para garantir que seja fechado
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return lista;
     }
 
 }
