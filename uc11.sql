ba9858cdbce62cc8873734be7b9781fee7a0563e
FelipeSouza95 <felipemoraes.recreio@gmail.com>
2024-08-19 11:03:24 -0300

    Alteração Script Banco de Dados

--- uc11.sql
+++ uc11.sql
@@ -1,31 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 27-Abr-2023 às 17:58
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `uc11`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `produtos`
---
+create database uc11;
+USE uc11;
 
 CREATE TABLE `produtos` (
   `id` bigint(20) UNSIGNED NOT NULL,
