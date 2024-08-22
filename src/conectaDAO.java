c6779f233eb9c4c841769832b19d40934ab4d3ef
FelipeSouza95 <felipemoraes.recreio@gmail.com>
2024-08-22 10:20:08 -0300

    Ajuste para conectar com banco de dados

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -10,8 +10,9 @@ public class conectaDAO {
         Connection conn = null;
 
         try {
-            // Adicionando useSSL=false à URL de conexão para desabilitar SSL
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?user=root&password=F010116m&useSSL=false");
+            // Adicionando allowPublicKeyRetrieval=true e serverTimezone à URL de conexão
+            String url = "jdbc:mysql://localhost:3306/uc11?user=root&password=F010116m&useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
+            conn = DriverManager.getConnection(url);
 
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO: " + erro.getMessage());
